35ef5b60ed51e44649920fdd739c7571e1310f71
rama2008 <70692885+rama2008@users.noreply.github.com>
2021-02-05 08:54:30 +0530

    Add files via upload

---
+++ Game.js
@@ -0,0 +1,95 @@
+class Game{
+    constructor(){
+
+    }
+    getState() {
+        var gameStateRef = database.ref('gameState');
+        gameStateRef.on("value", function (data) {
+            gameState = data.val();
+        })
+
+    }
+
+    update(state) {
+        database.ref('/').update({
+            gameState: state
+        });
+    }
+    async start() {
+            if (gameState === 0) {
+                player = new Player();
+                var playerCountRef = await database.ref('playerCount').once("value");
+                if (playerCountRef.exists()) {
+                    playerCount = playerCountRef.val();
+                    player.getCount();
+                }
+                form = new Form()
+                form.display();
+            }
+    player1 = createSprite(200,500);
+    player1.addImage("player1",player_img);
+    
+    player2 = createSprite(800,500);
+    player2.addImage("player2", player_img);
+    players=[player1,player2];
+
+        }
+    
+    play(){
+        
+        form.hide();
+
+        Player.getPlayerInfo();
+        image(back_img, 0, 0, 1000, 800);
+        var x =100;
+        var y=200;
+        var index =0;
+        drawSprites();
+
+        for(var plr in allPlayers){
+        
+            index = index+1;
+            x = 500-allPlayers[plr].distance;
+            y=500;
+            
+            players[index -1].x = x;
+            players[index - 1].y = y;
+
+            var index = 0;
+
+      //x and y position of the cars
+      var x = 200;
+      var y;
+
+      for(var plr in allPlayers){
+        //add 1 to the index for every loop
+        index = index + 1 ;
+
+        //position the cars a little away from each other in x direction
+        x = x + 200;
+        //use data form the database to display the cars in y direction
+        y = displayHeight - allPlayers[plr].distance;
+       
+      
+        //textSize(15);
+        //text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+      }
+
+    }
+
+    if(keyIsDown(UP_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+    }
+
+    if(player.distance>4000){
+      gameState=2;
+      game.update(2);
+         }
+    drawSprites();
+  }
+  end (){
+    console.log("game end")
+  }
+
+       
